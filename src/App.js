93402defe9fcd6338d11439b4a2c9ce43751a1b8
Jitesh Kumar <jiteshece@gmail.com>
2024-12-12 19:43:44 +0530

    Header added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import { Counter } from './features/counter/Counter';
+import './App.css';
+import Headers from './Components/Header/Header';
+
+function App() {
+  return (
+    <div className='App'>
+      <Headers />
+    </div>
+  );
+}
+
+export default App;
